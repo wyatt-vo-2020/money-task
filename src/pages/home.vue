6b74be7e57e05ff1a26268710ba26666231e08ae
Wyatt VO <votranquy96@gmail.com>
2023-09-02 16:51:36 +0700

    Note for computed, watch, watchEffect

--- src/pages/home.vue
+++ src/pages/home.vue
@@ -11,7 +11,7 @@
   <button @click="onChangeSomething">Click</button>
 </template>
 <script>
-import { watch, computed, ref, reactive } from "vue";
+import { watch, computed, ref, reactive, watchEffect } from "vue";
 export default {
   setup() {
     const searchText = ref("");
@@ -23,6 +23,9 @@ export default {
       "sala",
       "Someka",
     ]);
+    // computed gan giong methods
+    // computed khong nhan tham so dau vao nen chi tac dong len cac doi tuong co san trong data
+    // computed se thuc hien khi bien phu thuoc thay doi, con methods chi thuc hien khi duoc  goi
     const customersFilled = computed(() =>
       customers
         .map((customer) => {
@@ -31,8 +34,20 @@ export default {
         })
         .filter((customer) => customer.includes(searchText.value.toLowerCase()))
     );
+    // watch giam sat mot thuoc tinh nao do cua data
+    // watch co tham so
+    // watch thuc hien khi doi tuong giam sat thay doi
     watch(searchText, (newValue, oldValue) => {
       console.log(newValue, oldValue);
+    });
+    // watchEffect giong computed
+    // Ko nhan tham so dau vao ma track doi tuong nao do trong data
+    // Thuc hien khi bien phu thuoc thay doi
+    // Khac voi computed o cho trong return lai gia tri
+    watchEffect(() => {
+      if (searchText.value) {
+        console.log("run again");
+      }
     });
     return { searchText, customersFilled };
   },
