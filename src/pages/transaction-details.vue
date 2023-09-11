bc69beae5f67bdcf398db7149c33dbbd7c2a5a5e
Wyatt VO <votranquy96@gmail.com>
2023-09-11 22:57:39 +0700

    Change to use composition API

--- src/pages/transaction-details.vue
+++ src/pages/transaction-details.vue
@@ -8,25 +8,35 @@
   </div>
 </template>
 <script>
+import { computed } from "vue";
+import { useStore } from "vuex";
+import { useRoute } from "vue-router";
 export default {
   // data() {
   //   return {
   //     transaction: null,
   //   };
   // },
-  computed: {
-    transaction() {
-      return this.$store.state.transaction;
-    },
-    transactionsFilter() {
-      return this.$store.getters.transactionFiltered;
-    },
-  },
-  created() {
-    // fetch("http://localhost:3000/transaction/" + this.$route.params.id)
-    //   .then((response) => response.json())
-    //   .then((data) => (this.transaction = data));
-    this.$store.dispatch("fetchTraction", { id: this.$route.params.id });
+  // computed: {
+  //   transaction() {
+  //     return this.$store.state.transaction;
+  //   },
+  //   transactionsFilter() {
+  //     return this.$store.getters.transactionFiltered;
+  //   },
+  // },
+  // created() {
+  //   // fetch("http://localhost:3000/transaction/" + this.$route.params.id)
+  //   //   .then((response) => response.json())
+  //   //   .then((data) => (this.transaction = data));
+  //   this.$store.dispatch("fetchTraction", { id: this.$route.params.id });
+  // },
+  setup() {
+    const store = useStore();
+    const route = useRoute();
+    store.dispatch("fetchTraction", { id: route.params.id });
+    const transaction = computed(() => store.state.transaction);
+    return { transaction };
   },
 };
 </script>
