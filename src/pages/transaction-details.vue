a6602255350580b0d814e11404468719c8e455c7
Wyatt VO <votranquy96@gmail.com>
2023-09-11 16:03:45 +0700

    Vuex dispatch

--- src/pages/transaction-details.vue
+++ src/pages/transaction-details.vue
@@ -2,23 +2,31 @@
 <template>
   <h1>Transcation Detail Page</h1>
   <div v-if="transaction">
-    <h2>{{ transaction.id }}</h2>
+    <h2>{{ $route.params.id }}</h2>
     <h2>{{ transaction.name }}</h2>
     <h2>{{ transaction.price }}</h2>
   </div>
 </template>
 <script>
 export default {
-  data() {
-    return {
-      transaction: null,
-    };
+  // data() {
+  //   return {
+  //     transaction: null,
+  //   };
+  // },
+  computed: {
+    transaction() {
+      return this.$store.state.transaction;
+    },
+    transactionsFilter() {
+      return this.$store.getters.transactionFiltered;
+    },
   },
   created() {
-    fetch("http://localhost:3000/transaction/" + this.$route.params.id)
-      .then((response) => response.json())
-      .then((data) => (this.transaction = data))
-      .then(() => console.log(this.transaction));
+    // fetch("http://localhost:3000/transaction/" + this.$route.params.id)
+    //   .then((response) => response.json())
+    //   .then((data) => (this.transaction = data));
+    this.$store.dispatch("fetchTraction", { id: this.$route.params.id });
   },
 };
 </script>
